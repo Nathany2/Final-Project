5ae08459f9659a5254041bc5c92cecf694c1f750
Nathany2 <tashalum808@gmail.com>
2025-04-05 18:53:55 +0000

    Background

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ canvas.height = 576
 
 const gravity = 0.5
 
+class Sprite {
+    constructor({position, imageSrc}) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+}
+
+draw() {
+    if (!this.image) return
+    c.drawImage(this.image, this.position.x, this.position.y)
+}
+
+update() {
+    this.draw()
+}
+
 class Player {
     constructor(position)   {
         this.position = position
@@ -51,6 +68,14 @@ const keys = {
     pressed: false,
     },
 }
+
+const Background = new Sprite({
+    position: {
+        x: 0,
+        Y: 0,
+    }
+    //Background here
+})
 
 function animate() {
     window.requestAnimationFrame(animate)
